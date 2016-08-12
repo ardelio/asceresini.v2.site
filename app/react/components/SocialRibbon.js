4136bd2dce7669625342e6ed31db00d9e046f4db
Anthony Sceresini <asceresini@gmail.com>
2016-08-13 00:22:10 +1000

    Fix styling issues on IE

--- app/react/components/SocialRibbon.js
+++ app/react/components/SocialRibbon.js
@@ -4,31 +4,36 @@ import { SocialIcon } from '../components'
 class SocialRibbon extends Component {
   render() {
     return (
-      <div id="social-ribbon-component" className="grid justify-content -around">
+      <div id="social-ribbon-component" className="grid">
+        <div className="cell -1of12" />
         <div className="cell -1of12">
-        <SocialIcon
-        alt="Github"
-        src="github.svg"
-        url="https://github.com/asceresini" />
-        </div>
+          <SocialIcon
+            alt="Github"
+            src="github.svg"
+            url="https://github.com/asceresini" />
+          </div>
+        <div className="cell -2of12" />
         <div className="cell -1of12">
           <SocialIcon
             alt="Twitter"
             src="twitter.svg"
             url="https://twitter.com/asceresini" />
         </div>
+        <div className="cell -2of12" />
         <div className="cell -1of12">
           <SocialIcon
             alt="Linkedin"
             src="linkedin.svg"
             url="http://au.linkedin.com/in/asceresini/" />
         </div>
+        <div className="cell -2of12" />
         <div className="cell -1of12">
           <SocialIcon
             alt="Facebook"
             src="facebook.svg"
             url="https://www.facebook.com/ardelio" />
         </div>
+        <div className="cell -1of12" />
       </div>
     )
   }
